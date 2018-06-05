1c709e59a852f23e8933030dbd428755a373babe
Unknown <jvander2@ncsu.edu>
2018-06-05 13:23:55 -0400

    Added boilerplate for UI

---
+++ ui/src/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        Welcome
+      </div>
+    );
+  }
+}
+
+export default Header;
